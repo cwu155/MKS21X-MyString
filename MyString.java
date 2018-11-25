e8940f6d96d685f623a2e0e985fa67d9446621c1
cwu155 <cwu17@stuy.edu>
2018-11-24 23:58:01 -0500

    Added empty methods??

---
+++ MyString.java
@@ -0,0 +1,38 @@
+public class MyString implements CharSequence,Comparable<CharSequence>{
+  //Need to throw exceptions
+  private char[] data;
+  public MyString(CharSequence s){
+    data = new char[s.length()];
+    for (int i = 0; i < s.length(); i++){
+      data[i] = s.charAt(i);
+    }
+  }
+
+  //Methods
+
+  public int length(){
+    int count = 0;
+    for (int i = 0; i < data.length; i++){
+      count += 1;
+    }
+    return count;
+  }
+
+  public char charAt(int index){
+  }
+
+  public CharSequence subSequence(int a, int b){
+  }
+
+  public String toString(){
+
+  }
+
+  public int compareTo(CharSequence s){
+
+  }
+
+
+
+
+}
